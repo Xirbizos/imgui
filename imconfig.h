2f9aa9f0e676d00dd00b2f3e92a0ca6617f4c59f
Manuel Schenk <xirbizos@gmail.com>
2024-01-29 17:54:48 +0100

    Add imvec to glmvec conversion

--- imconfig.h
+++ imconfig.h
@@ -129,3 +129,13 @@ namespace ImGui
     void MyFunction(const char* name, MyMatrix44* mtx);
 }
 */
+#include <glm/glm.hpp>
+// define extra conversion here before including imgui, don't do it in the imconfig.h
+#define IM_VEC2_CLASS_EXTRA \
+    constexpr ImVec2(glm::vec2& f) : x(f.x), y(f.y) {} \
+    operator glm::vec2() const { return glm::vec2(x, y); }
+
+#define IM_VEC4_CLASS_EXTRA \
+        constexpr ImVec4(const glm::vec4& f) : x(f.x), y(f.y), z(f.z), w(f.w) {} \
+        operator glm::vec4() const { return glm::vec4(x,y,z,w); }
+
